a97a58b1e358bbb9e90ff080fc31110540078f5c
Joenz Kim-Chan <jokichaa@gmail.com>
2021-05-28 18:56:53 +0800

    Remove some unnecessary comments

--- application/src/Components/TimeForm.js
+++ application/src/Components/TimeForm.js
@@ -7,21 +7,13 @@ const TimeForm = ({ time, setTime }) => {
       <h6>Choose the semester</h6>
       <YearForm setTime={setTime} time={time} />
       <SemForm setTime={setTime} time={time} />
-      {/* <Row>
-        <Col lg={2}>
-          <YearForm setTime={setTime} time={time} />
-        </Col>
-        <Col lg={2}>
-          <SemForm setTime={setTime} time={time} />
-        </Col>
-      </Row> */}
     </div>
   );
 };
 
 const YearForm = ({ setTime, time }) => {
   const handleChange = (input) => {
-    console.log(input)
+    console.log(input);
     setTime({
       year: input,
       sem: time.sem,
@@ -29,28 +21,12 @@ const YearForm = ({ setTime, time }) => {
   };
 
   return (
-    // <form>
-    //     Year:
-    //     <select id="YearInput" onChange={handleChange}>
-    //         <option value="2021">2021</option>
-    //         <option value="2020">2020</option>
-    //     </select>
-    // </form>
     <Dropdown>
       <DropdownButton title={time.year} onSelect={handleChange}>
         <Dropdown.Item eventKey="2020-2021">2020-2021</Dropdown.Item>
         <Dropdown.Item eventKey="2019-2020">2019-2020</Dropdown.Item>
       </DropdownButton>
     </Dropdown>
-    // <Dropdown>
-    //   <Dropdown.Toggle variant="Primary" id="dropdown-basic" onSelect={handleChange}>
-    //     Year
-    //   </Dropdown.Toggle>
-    //   <Dropdown.Menu >
-    //     <Dropdown.Item eventKey="2021">2021</Dropdown.Item>
-    //     <Dropdown.Item eventKey="2020">2020</Dropdown.Item>
-    //   </Dropdown.Menu>
-    // </Dropdown>
   );
 };
 
@@ -62,13 +38,6 @@ const SemForm = ({ setTime, time }) => {
     });
   };
   return (
-    // <form>
-    //     Sem:
-    //     <select id="SemInput" onChange={handleChange}>
-    //         <option value="1">1</option>
-    //         <option value="2">2</option>
-    //     </select>
-    // </form>
     <Dropdown>
       <DropdownButton title={time.sem} onSelect={handleChange}>
         <Dropdown.Item eventKey="1">1</Dropdown.Item>
